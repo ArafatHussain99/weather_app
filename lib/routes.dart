4cf0be840c29795e7b2eaae39d61419de46f511f
arafatboss123 <arafatsifat10@gmai.com>
2024-07-03 15:40:56 +0600

    [Added]: Profile screen added

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/notifications/view/notification_screen.dart';
+import 'package:weather_app/profile/view/profile_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
-    
     case NotificationScreen.id:
       return MaterialPageRoute(
         settings: routeSettings,
-        builder: (_) =>const  NotificationScreen(),
+        builder: (_) => const NotificationScreen(),
+      );
+    case ProfileScreen.id:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const ProfileScreen(),
       );
     default:
       return MaterialPageRoute(
