c3b2b57a8e5ac1d188866a3ee7f4e31028165dac
arafatboss123 <arafatsifat10@gmai.com>
2024-07-03 17:18:52 +0600

    [Add]: 'Add new city' button added add dummy cities added.

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:weather_app/global_provider.dart';
+import 'package:weather_app/home/view/widgets/cities_weather.dart';
 import 'package:weather_app/home/view/widgets/drawer_other_details.dart';
 import 'package:weather_app/home/view/widgets/drawer_profile_info.dart';
 import 'package:weather_app/home/view/widgets/zaynax_logo.dart';
 import 'package:weather_app/notifications/view/notification_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreenState extends ConsumerState {
+  List cities = ['Dhaka', 'Khulna', 'Mumbai', 'London', 'Tottenham'];
+  TextEditingController newCity = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +45,114 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ZaynaxLogo(
-              size: MediaQuery.of(context).size.width / 2.5,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ZaynaxLogo(
+                size: MediaQuery.of(context).size.height / 5,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    isScrollControlled:
+                        true, // Make the bottom sheet scrollable
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom,
+                        ),
+                        child: SingleChildScrollView(
+                          child: SizedBox(
+                            height: 200,
+                            child: Center(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const Text('Add a new city'),
+                                    const SizedBox(
+                                      height: 12,
+                                    ),
+                                    TextField(
+                                      decoration: const InputDecoration(
+                                        border: OutlineInputBorder(),
+                                      ),
+                                      controller: newCity,
+                                    ),
+                                    const SizedBox(
+                                      height: 12,
+                                    ),
+                                    ElevatedButton(
+                                      child: const Text('Submit'),
+                                      onPressed: () {
+                                        setState(() {
+                                          cities.add(newCity.text);
+                                        });
+
+                                        Navigator.pop(context);
+                                        Fluttertoast.showToast(
+                                          msg: 'New city added!',
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.BOTTOM,
+                                          timeInSecForIosWeb: 1,
+                                          backgroundColor: Colors.black54,
+                                          textColor: Colors.white,
+                                          fontSize: 16.0,
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                  setState(() {});
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: const Center(child: Text('Add new city')),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height -
+                    MediaQuery.of(context).size.height / 5 -
+                    16 -
+                    46,
+                child: RefreshIndicator(
+                  onRefresh: () async {
+                    ref.read(refresh.notifier).state = ref.read(refresh) + 1;
+                  },
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: cities.length,
+                    itemBuilder: (context, index) {
+                      return CitiesWeatherWidget(city: cities[index]);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       drawer: const Drawer(
@@ -55,7 +163,7 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 12,
             ),
-            DrawerOtherDetails()
+            DrawerOtherDetails(),
           ],
         ),
       ),
