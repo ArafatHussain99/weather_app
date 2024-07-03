c616c6c7f4067772b49dfd422eccb985041c3c88
arafatboss123 <arafatsifat10@gmai.com>
2024-07-03 17:17:40 +0600

    [Add]: Api endpoints added

---
+++ lib/core/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndpoints {
+  // receiveTimeout
+  static const String baseUrl =
+      'https://api.openweathermap.org/data/2.5/weather?q=';
+  static const appid = '0d1a1c371852b0762b8245f17951fff0';
+}
